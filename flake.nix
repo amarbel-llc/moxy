2817f019dc6f0a329f2032239ca66f75771930e9
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2026-04-21 10:06:53 -0400

    chore: release v0.4.8

--- flake.nix
+++ flake.nix
@@ -111,7 +111,7 @@
           '';
         };
 
-        moxyVersion = "0.4.7";
+        moxyVersion = "0.4.8";
         moxyCommit = self.shortRev or self.dirtyShortRev or "unknown";
 
         # Man pages as a standalone derivation, referenced by both the moxy
